2ea4899059aa6346b330179daf361120ced694b9
Rawisha <rawvsmaw22@gmail.com>
2023-12-04 15:00:40 +0100

    auto import fix

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,10 +2,10 @@ module.exports = {
   root: true,
   extends: ['plugin:@next/next/recommended', '@payloadcms'],
   ignorePatterns: ['**/payload-types.ts'],
-  plugins: ['prettier'],
+  plugins: ['prettier', 'simple-import-sort'],
   rules: {
-    "prettier/prettier": [
-      'error', {endOfLine: 'auto'}
-    ]
-  }
+    'prettier/prettier': ['error', { endOfLine: 'auto' }],
+    'simple-import-sort/imports': 'error',
+    'simple-import-sort/exports': 'error',
+  },
 }
