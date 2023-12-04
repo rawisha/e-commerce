99ff51a84f85b41880bde8698c99550e282a2217
Rawisha <rawvsmaw22@gmail.com>
2023-12-04 13:38:21 +0100

    finished

---
+++ src/app/(pages)/account/layout.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { Gutter } from "../../_components/Gutter";
+
+import classes from './index.module.scss'
+import { profileNavItems } from "../../constants";
+import Link from "next/link";
+import Image from "next/image";
+
+export default function Layout({children}: {children: React.ReactNode}){
+return(
+    <div className={classes.container}>
+        <Gutter>
+            <h3>My Profile</h3>
+            <div className={classes.account}>
+                <div className={classes.nav}>
+
+                    <ul>
+                        {profileNavItems.map((item)=>(
+                            <li key={item.title}>
+                                <Link href={item.url} className={classes.navItem}>
+                                    <Image src={item.icon} alt={item.title} width={24} height={24}/>
+                                    <p>{item.title}</p>
+                                </Link>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+                {children}
+            </div>
+        </Gutter>
+    </div>
+)
+}
--- src/app/_components/Header/Nav/index.tsx
+++ src/app/_components/Header/Nav/index.tsx
@@ -5,31 +5,32 @@ import Link from 'next/link'
 
 import { Header as HeaderType } from '../../../../payload/payload-types'
 import { useAuth } from '../../../_providers/Auth'
+import { Button } from '../../Button'
 import { CartLink } from '../../CartLink'
 import { CMSLink } from '../../Link'
 
 import classes from './index.module.scss'
-import { Button } from '../../Button'
 
 export const HeaderNav: React.FC<{ header: HeaderType }> = ({ header }) => {
   const navItems = header?.navItems || []
   const { user } = useAuth()
 
   return (
-    <nav
-      className={[
-        classes.nav,
-        user === undefined && classes.hide,
-      ]
-        .filter(Boolean)
-        .join(' ')}
-    >
+    <nav className={[classes.nav, user === undefined && classes.hide].filter(Boolean).join(' ')}>
       {navItems.map(({ link }, i) => {
         return <CMSLink key={i} {...link} appearance="none" />
       })}
-      <CartLink />
+      {!user && <CartLink />}
       {user && <Link href="/account">Account</Link>}
-      {!user && <Button el='link' href='/login' label='Login' appearance='primary' onClick={() => (window.location.href = '/login')} />}
+      {!user && (
+        <Button
+          el="link"
+          href="/login"
+          label="Login"
+          appearance="primary"
+          onClick={() => (window.location.href = '/login')}
+        />
+      )}
       {user && <CartLink />}
     </nav>
   )
