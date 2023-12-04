e321af9111b5881acf122ef31fb01c309807dce2
Rawisha <rawvsmaw22@gmail.com>
2023-12-04 15:05:47 +0100

    ignore eslint

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ const ContentSecurityPolicy = require('./csp')
 const redirects = require('./redirects')
 
 const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   reactStrictMode: true,
   swcMinify: true,
   images: {
